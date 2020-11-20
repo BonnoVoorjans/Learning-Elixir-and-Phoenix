d8ed995883796172916b33b0d6e0fa987207d019
Bonno Voorjans <bcv_neo@hotmail.com>
2020-11-20 16:30:17 +0100

    Added counter to licenses

--- live_view/course_code/lib/live_view_studio_web/live/license_live.ex
+++ live_view/course_code/lib/live_view_studio_web/live/license_live.ex
@@ -4,7 +4,18 @@ defmodule LiveViewStudioWeb.LicenseLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 3, amount: LicenseCalculator.calc(3))
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :tick)
+    end
+    
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
+    socket = 
+      assign( socket, 
+              seats: 3, 
+              amount: LicenseCalculator.calc(3), 
+              expiration_time: expiration_time,
+              time_remaining: time_remaining(expiration_time))
     {:ok, socket}
   end
 
@@ -19,5 +30,22 @@ defmodule LiveViewStudioWeb.LicenseLive do
       )
 
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, 
+                    time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
